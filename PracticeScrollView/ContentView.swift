53bb3f3682d6ede091a7b9b79a7551526aef5439
石川太洋 <taiyoishikawa@MacBook-Pro-2.local>
2020-09-10 20:59:06 +0900

    sampleの追加

--- PracticeScrollView/ContentView.swift
+++ PracticeScrollView/ContentView.swift
@@ -19,54 +19,100 @@ struct ContentView: View {
 					Text("")
 						.navigationBarTitle(Text("ScrollViewSample").font(.subheadline), displayMode: .large)
 					VStack {
-						//Firstの部分ScrollView
-						Text("First")
-							.font(.largeTitle)
-							.bold()
-							.frame(width: 340, height: 20, alignment: .leading)
-						ScrollView(.horizontal) {
-							HStack(spacing: 30) {
-								ForEach(0..<30) {num in
-									//cornerRadiusなし
-									FirstCellView(num: num)							.foregroundColor(Color.orange)
-										.border(Color.orange, width: 2)
+						ScrollView(.vertical) {
+							//Firstの部分ScrollView
+							Text("First")
+								.font(.title)
+								.bold()
+								.frame(width: 340, height: 20, alignment: .leading)
+							ScrollView(.horizontal) {
+								HStack(spacing: 30) {
+									ForEach(0..<30) {num in
+										//cornerRadiusなし
+										FirstCellView(num: num)
+											.foregroundColor(Color.orange)
+											.border(Color.orange, width: 2)
+									}
 								}
-							}
-						}.padding(.leading, 10)
-						Spacer().frame(height: 50)
-						
-						//Secondの部分ScrollView
-						Text("Second")
-							.font(.largeTitle)
-							.bold()
-							.frame(width: 340, height: 20, alignment: .leading)
-						ScrollView(.horizontal) {
-							HStack(spacing: 20) {
-								ForEach(0..<30) {num in
-									//cornerRadiusありの場合の書き方
-									SecondCellView(num: num)
-										.foregroundColor(Color.purple)
-										.overlay(
-											RoundedRectangle(cornerRadius: 50)
-												.stroke(Color.purple, lineWidth: 1)
-									)
+							}.padding(.leading, 10)
+							Spacer().frame(height: 30)
+							
+							//Secondの部分ScrollView
+							Text("Second")
+								.font(.title)
+								.bold()
+								.frame(width: 340, height: 20, alignment: .leading)
+							ScrollView(.horizontal) {
+								HStack(spacing: 20) {
+									ForEach(0..<30) {num in
+										//cornerRadiusありの場合の書き方
+										SecondCellView(num: num)
+											.foregroundColor(Color.purple)
+											.overlay(
+												RoundedRectangle(cornerRadius: 50)
+													.stroke(Color.purple, lineWidth: 1)
+										)
+									}
 								}
-							}
-						}.padding(.leading, 10)
-						Spacer().frame(height: 50)
-						
-						//Thirdの部分のScrollView(画像を用いる)
-						Text("Third")
-							.font(.largeTitle)
-							.bold()
-							.frame(width: 340, height: 20, alignment: .leading)
-						ScrollView(.horizontal) {
-							HStack(spacing: 20) {
-								ForEach(0..<30) {_ in
-									ThirdCellView()
+							}.padding(.leading, 10)
+							Spacer().frame(height: 30)
+							
+							//Thirdの部分のScrollView(画像を用いる)
+							Text("Third")
+								.font(.title)
+								.bold()
+								.frame(width: 340, height: 20, alignment: .leading)
+							ScrollView(.horizontal) {
+								HStack(spacing: 20) {
+									ForEach(0..<30) {_ in
+										ThirdCellView()
+									}
 								}
+							}.padding(.leading, 10)
+							
+							Spacer().frame(height: 30)
+							
+							//同じような記述が続く場合はGroupで記述してあげればエラーは起きない
+							Group {
+								//以下縦スクロール用のサンプル
+								Text("sample")
+									.font(.title)
+									.bold()
+									.frame(width: 340, height: 20, alignment: .leading)
+								ScrollView(.horizontal) {
+									HStack(spacing: 20) {
+										ForEach(0..<30) {_ in
+											ThirdCellView()
+										}
+									}
+								}.padding(.leading, 10)
+								
+								Text("sample")
+									.font(.title)
+									.bold()
+									.frame(width: 340, height: 20, alignment: .leading)
+								ScrollView(.horizontal) {
+									HStack(spacing: 20) {
+										ForEach(0..<30) {_ in
+											ThirdCellView()
+										}
+									}
+								}.padding(.leading, 10)
+								
+								
+								Text("sample")
+									.font(.title)
+									.bold()
+									.frame(width: 340, height: 20, alignment: .leading)
+								ScrollView(.horizontal) {
+									HStack(spacing: 20) {
+										ForEach(0..<30) {_ in
+											ThirdCellView()
+										}
+									}
+								}.padding(.leading, 10)
 							}
-						}.padding(.leading, 10)
+						}
 					}
 				}
 			}
